e939b4e0fe6058db5c1c94e701f3ef3e7786f757
ilakr114 <108058618+ilakr114@users.noreply.github.com>
2022-09-02 07:10:56 -0700

    Update Activity3.java

--- Selenium/Project/Activity3.java
+++ Selenium/Project/Activity3.java
@@ -4,32 +4,40 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Activity2 {
+public class Activity3 {
 WebDriver driver;
-
 
 @BeforeMethod
 
 public void beforeMethod() {
-	
 	driver = new FirefoxDriver();
-	driver.get("http://alchemy.hguy.co/orangehrm");	
+	
+	driver.get("http://alchemy.hguy.co/orangehrm");
+	
 }
-
-
+	
 @Test
 
-public void displayUrl() {
+public void login() {
 	
+	WebElement username = driver.findElement(By.id("txtUsername"));
+	username.sendKeys("orange");
 	
-	WebElement img = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div[1]/img"));
+	WebElement password = driver.findElement(By.id("txtPassword"));
+	password.sendKeys("orangepassword123");
 	
-	System.out.println("The URL is :" +img.getAttribute("src"));
+	WebElement submit = driver.findElement(By.id("btnLogin"));
+	submit.click();
+	
+	String currentURL = driver.getCurrentUrl();
+    Assert.assertEquals("http://alchemy.hguy.co:8080/orangehrm/symfony/web/index.php/dashboard",currentURL);
 		
+    System.out.println("HEYY I AM IN HOME PAGE");
 }
 
 @AfterMethod
@@ -38,6 +46,4 @@ public void afterMethod() {
 	
 	driver.quit();
 }
-
-
 }
